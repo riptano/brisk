a04f3cd2dd184122878aeb4ad5401b2c1deeff9c
Brandon Williams <brandon@datastax.com>
2011-04-11 15:21:37 -0500

    preliminary pig support

--- bin/brisk-env.sh
+++ bin/brisk-env.sh
@@ -22,8 +22,12 @@ export HADOOP_CLASSPATH=$CLASSPATH
 export HADOOP_HOME=`pwd`/`dirname $0`/../resources/hadoop
 #export HADOOP_LOG_DIR=
 
-#export PIG_HOME=
+export PIG_HOME=`dirname $0`/../resources/pig
 export PIG_CLASSPATH=$HADOOP_HOME/conf:$CLASSPATH
+# pig also needs some of hadoop's dependencies
+for jar in `ls $HADOOP_HOME/*.jar $HADOOP_HOME/lib/*.jar`; do
+    PIG_CLASSPATH=$PIG_CLASSPATH:$jar
+done
 
 export HIVE_HOME=`dirname $0`/../resources/hive
 
